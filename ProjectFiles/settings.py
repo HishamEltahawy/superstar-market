b49349ad59f36cdc37f19652f59a5df414dde892
Hisham Eltahawy <hishameltahawy555@gmail.com>
2025-05-22 18:13:39 +0300

    edit requirements file

--- ProjectFiles/settings.py
+++ ProjectFiles/settings.py
@@ -6,21 +6,15 @@ from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 
 ENCRYPTION_KEY = os.environ.get('ENCRYPTION_KEY', Fernet.generate_key())
 
-# SECRET_KEY = os.environ.get("SECRET_KEY", "default-secret-key")
 DEBUG = os.environ.get("DEBUG")
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
-
 SECRET_KEY = ENCRYPTION_KEY
+
 # DEBUG = True
 # ALLOWED_HOSTS = ['localhost']
 
@@ -123,15 +117,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ProjectFiles.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql.psycopg2',
